5ba6858ad5cf40855d7004f5f2c0bbdb5e1d499a
Daniel Carazo <dhca09@hotmail.com>
2025-10-28 21:17:32 -0600

    day 14, higher lower game, using functions and external data + try catch
    Signed-off-by: Daniel Carazo <dhca09@hotmail.com>

---
+++ Day14_High_Low/main.py
@@ -0,0 +1,18 @@
+from art import logo
+from art import vs
+from data import data
+
+
+
+
+
+
+
+try:
+    age = int(input("How old are you?"))
+except ValueError:
+    print("You have typed in a an invalid number. Please try again with a numerical response such as 15.")
+    age = int(input("How old are you?"))
+
+if age > 18:
+    print(f"You can drive at age {age}.")
