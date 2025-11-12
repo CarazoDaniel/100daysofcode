21dfb4ac495977f08b86d9920dba6f30ee79c9b2
Daniel Carazo <dhca09@hotmail.com>
2025-11-11 19:52:47 -0600

    adding the brains, reviewing the answer, the question and keeping score
    Signed-off-by: Daniel Carazo <dhca09@hotmail.com>

---
+++ Day17_Quizz_Game/quiz_brain.py
@@ -0,0 +1,23 @@
+class QuizBrain:
+    """Makes the core for the quizz"""
+    def __init__(self,quizz_list):
+        self.question_number = 0
+        self.question_list = quizz_list
+        self.score = 0
+    def still_has_questions(self):
+        return self.question_number < len(self.question_list)
+    
+    def check_answer(self,user_input,question_answer):
+        if user_input.lower() == question_answer.lower():
+            print('You got it right')
+            self.score += 1
+        else:
+            print('oh shoot, you missed')
+        print(f'the correct answer is: {question_answer}')
+        print(f'Your score is: {self.score}/{self.question_number+1}')
+        print("\n")
+        
+    def next_question(self):
+        user_answer = input(f"Q.{self.question_number + 1}: {self.question_list[self.question_number].text} (True/False)?: ")
+        self.check_answer(user_answer,self.question_list[self.question_number].answer)
+        self.question_number += 1
