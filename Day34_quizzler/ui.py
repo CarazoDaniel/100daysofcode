100fdb02eff7897062ee0f4731d275de68848c7e
Daniel Carazo Arias <dhca09@hotmail.com>
2026-03-30 11:23:44 -0600

    adding day33, the ISS API tracker notificator. This uses gmail API to send emails from a personal account and day 34, using the open tdb to update the quizz game from day 17, having a datajson call directly from the API
    Signed-off-by: Daniel Carazo Arias <dhca09@hotmail.com>

---
+++ Day34_quizzler/ui.py
@@ -0,0 +1,59 @@
+from tkinter import *
+from quiz_brain import QuizBrain
+
+THEME_COLOR = "#375362"
+
+
+class QuizInterface:
+    def __init__(self,quiz_brain:QuizBrain):
+        self.quiz = quiz_brain
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20,pady=20,bg=THEME_COLOR,)
+        
+        self.false_image = PhotoImage(file="./images/false.png")
+        self.true_image = PhotoImage(file="./images/true.png")
+        
+        self.false_button = Button(image=self.false_image, highlightthickness=0,command=self.false_pressed)
+        self.false_button.grid(row=2,column=1,padx=10,pady=10)
+
+        self.true_button = Button(image=self.true_image, highlightthickness=0,command=self.true_pressed)
+        self.true_button.grid(row=2,column=0,padx=10,pady=10)
+        
+        self.score = Label(text="Score: 0",padx=20,pady=20,highlightthickness=0,bg=THEME_COLOR)
+        self.score.grid(row=0, column=1)
+        
+        self.canvas = Canvas(width=300,height=250,highlightthickness=0,bg="White")
+        self.question = self.canvas.create_text(150,125,
+                                                width=280,
+                                                text= "", 
+                                                font=("arial",15,"italic"),
+                                                fill=THEME_COLOR)
+        self.canvas.grid(row=1,column=0,columnspan=2,pady=50)
+        
+        self.get_next_question()
+        
+        self.window.mainloop()
+    
+    def get_next_question(self):
+        self.canvas.config( bg= 'White')
+        if self.quiz.still_has_questions():
+            self.score.config(text=f"Score: {self.quiz.score}")
+            q_text = self.quiz.next_question()
+            self.canvas.itemconfig(self.question, text = q_text)
+        else:
+            self.canvas.itemconfig(self.question, text = "You've reached the end of the quiz.")
+            self.false_button.config(state="disabled")
+            self.true_button.config(state="disabled")
+    def true_pressed(self):
+        self.give_feedback(self.quiz.check_answer("True"))
+    def false_pressed(self):
+        self.give_feedback(self.quiz.check_answer("False"))
+        
+    def give_feedback(self,is_right):
+        if is_right:
+            self.canvas.config(bg= 'Green')
+        else:
+            self.canvas.config(bg= 'Red')
+        self.window.after(1000,self.get_next_question)
+        
